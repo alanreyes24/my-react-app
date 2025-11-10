e04d33a7b8b8e17cc2d55cc0ac45d441b5e89d78
Alan Reyes <alanreyes6747@gmail.com>
2025-11-10 16:14:38 -0600

    add onSquareClick prop pass

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import { useState } from "react";
 
-function Square({ value }) {
+function Square({ value, onSquareClick }) {
   return (
     <button className="square" onClick={onSquareClick}>
       {value}
     </button>
   );
+}
 }
 
 export default function Board() {
